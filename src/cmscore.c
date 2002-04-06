be728a91b7b35ccad1fb4218d36f9ed1e432a6c9
Sean Eddy <sean@eddylab.org>
2002-04-06 19:09:30 +0000

    modified cmscore to do both full and d&c; full can be switched off with --smallonly option.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/trunk/infernal@757 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/cmscore.c
+++ src/cmscore.c
@@ -19,24 +19,24 @@
 #include "msa.h"                /* squid's multiple alignment i/o       */
 #include "stopwatch.h"          /* squid's process timing module        */
 
-static char banner[] = "cmscore - score an RNA covariance model against sequences";
+static char banner[] = "cmscore - score RNA covariance model against sequences";
 
 static char usage[]  = "\
 Usage: cmscore [-options] <cmfile> <sequence file>\n\
 The sequence file is expected to be in FASTA format.\n\
   Available options are:\n\
    -h     : help; print brief help on version and usage\n\
-   -B     : Babelfish; autodetect alternative sequence file format\n\
 ";
 
 static char experts[] = "\
    --informat <s>: specify that input alignment is in format <s>, not FASTA\n\
+   --smallonly   : do only d&c, don't do full CYK/inside\n\
 ";
 
 static struct opt_s OPTIONS[] = {
   { "-h", TRUE, sqdARG_NONE }, 
-  { "-B", TRUE, sqdARG_NONE },
-  { "--informat",  FALSE, sqdARG_STRING },
+  { "--informat",   FALSE, sqdARG_STRING },
+  { "--smallonly",  FALSE, sqdARG_NONE },
 };
 #define NOPTIONS (sizeof(OPTIONS) / sizeof(struct opt_s))
 
@@ -56,6 +56,8 @@ main(int argc, char **argv)
   float            sc1,  sc2;	/* score of a sequence */
   Parsetree_t     *tr1, *tr2;	/* a traceback */
   
+  int   do_smallonly;		/* TRUE to do only d&c, not full CYK/inside */
+
   char *optname;                /* name of option found by Getopt()        */
   char *optarg;                 /* argument found by Getopt()              */
   int   optind;                 /* index in argv[]                         */
@@ -64,12 +66,13 @@ main(int argc, char **argv)
    * Parse command line
    ***********************************************/
 
-  format            = SQFILE_FASTA;
+  format            = SQFILE_UNKNOWN;
+  do_smallonly      = FALSE;
   
   while (Getopt(argc, argv, OPTIONS, NOPTIONS, usage,
                 &optind, &optname, &optarg))  {
-    if      (strcmp(optname, "-B") == 0)          format            = SQFILE_UNKNOWN;
-    else if (strcmp(optname, "--informat") == 0) {
+    if      (strcmp(optname, "--smallonly") == 0) do_smallonly = TRUE;
+    else if (strcmp(optname, "--informat")  == 0) {
       format = String2SeqfileFormat(optarg);
       if (format == SQFILE_UNKNOWN) 
 	Die("unrecognized sequence file format \"%s\"", optarg);
@@ -113,29 +116,38 @@ main(int argc, char **argv)
       
       dsq = DigitizeSequence(seq, sqinfo.len);
 
-#if 0
-      StopwatchZero(watch);
-      StopwatchStart(watch);
-      sc1 = CYKInside(cm, dsq, sqinfo.len, &tr1);  
-      ParsetreeDump(stdout, tr1, cm, dsq);
-      printf("%-12s : %.2f  %.2f\n", sqinfo.name, sc1/0.693,
-	     ParsetreeScore(cm, tr1, dsq)/0.693);
-      StopwatchStop(watch);
-      StopwatchDisplay(stdout, "CPU time: ", watch);
-#endif
+      if (! do_smallonly) {
+	printf("Full inside algorithm:\n");
+	printf("----------------------\n");
+	StopwatchZero(watch);
+	StopwatchStart(watch);
+	sc1 = CYKInside(cm, dsq, sqinfo.len, &tr1);  
+	ParsetreeDump(stdout, tr1, cm, dsq);
+	printf("%-12s : %.2f  %.2f\n", sqinfo.name, sc1/0.693,
+	       ParsetreeScore(cm, tr1, dsq)/0.693);
+	StopwatchStop(watch);
+	StopwatchDisplay(stdout, "CPU time: ", watch);
+	puts("");
+      }
 
+      printf("Divide and conquer algorithm:\n");
+      printf("-------------------------------\n");
       StopwatchZero(watch);
       StopwatchStart(watch);
       sc2 = CYKDivideAndConquer(cm, dsq, sqinfo.len, &tr2);  
       ParsetreeDump(stdout, tr2, cm, dsq);
       printf("%-12s : %.2f  %.2f\n", sqinfo.name, sc2/0.693,
 	     ParsetreeScore(cm, tr2, dsq)/0.693);
-      /* ParsetreeCompare(tr1, tr2);  */
       StopwatchStop(watch);
       StopwatchDisplay(stdout, "CPU time: ", watch);
+      puts("");
+
+      if (! do_smallonly) {
+	ParsetreeCompare(tr1, tr2);  
+	FreeParsetree(tr1);  
+      }
 
       FreeSequence(seq, &sqinfo);
-      /*      FreeParsetree(tr1);  */
       FreeParsetree(tr2); 
       free(dsq);
     }
