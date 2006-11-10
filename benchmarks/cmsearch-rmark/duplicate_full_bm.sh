10e065f47f50c91c35a395784e8e861c741155cb
Eric Nawrocki <eric.nawrocki@gmail.com>
2006-11-10 16:56:43 +0000

    Added 'benchmarks' in the main infernal/ directory. Inside that is a cmsearch-rmark directory with 'rmark-1' the specific benchmark used in the banded-cyk manuscript and a 00README that hopefully is sufficient (it took a while to make...)
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/infernal/trunk@1684 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ benchmarks/cmsearch-rmark/duplicate_full_bm.sh
@@ -0,0 +1,20 @@
+# See 00README for details.
+# These 6 rmark_clusterfy calls will create 6 subdirectories in the current directory, named:
+#  inf-55_rmark-1_out_dir/
+#  inf_p1_noent-71_rmark-1_out_dir/
+#  inf_p1-71_rmark-1_out_dir/
+#  inf_noent-71_rmark-1_out_dir/
+#  inf-71_rmark-1_out_dir/
+#  inf_qdb-71_rmark-1_out_dir/
+# 
+# Inside each dir there will be two scripts *.com and *pp.sh. To submit the jobs 
+# to the cluster, run the *.com script. Then wait for all the jobs to finish and
+# run the *pp.sh script.
+
+perl rmark_clusterfy.pl infernal_55_W.rmm rmk_files/inf-55.rmk          rmark-1/  rmark-1/rmark-1.idx rmark-1 inf-55
+perl rmark_clusterfy.pl infernal.rmm      rmk_files/inf_p1_noent-71.rmk rmark-1/  rmark-1/rmark-1.idx rmark-1 inf_p1_noent-71
+perl rmark_clusterfy.pl infernal.rmm      rmk_files/inf_p1-71.rmk       rmark-1/  rmark-1/rmark-1.idx rmark-1 inf_p1-71
+perl rmark_clusterfy.pl infernal.rmm      rmk_files/inf_noent-71.rmk    rmark-1/  rmark-1/rmark-1.idx rmark-1 inf_noent-71
+perl rmark_clusterfy.pl infernal.rmm      rmk_files/inf-71.rmk          rmark-1/  rmark-1/rmark-1.idx rmark-1 inf-71
+perl rmark_clusterfy.pl infernal.rmm      rmk_files/inf_qdb-71.rmk      rmark-1/  rmark-1/rmark-1.idx rmark-1 inf_qdb-71
+
