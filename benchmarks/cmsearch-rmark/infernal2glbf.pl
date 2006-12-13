3b73d7e4d877dfecc64b1ca2ac7f24c4b1aa9543
Eric Nawrocki <eric.nawrocki@gmail.com>
2006-12-13 11:41:39 +0000

    Updated infernal2glbf.pl and infernal.rmm to read new cmsearch output and pick up E values. Added rmark_MPI_cmsearch.pl to handle MPI cluster runs.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/infernal/trunk@1758 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- benchmarks/cmsearch-rmark/infernal2glbf.pl
+++ benchmarks/cmsearch-rmark/infernal2glbf.pl
@@ -14,8 +14,9 @@
 #
 #
 # Options from :
-#    -E <x>         : use E values [default], sets max E-val to keep as <x> [default = 2]
+#    -E <x>         : use E values [default], sets max E-val to keep as <x> [default = 10]
 #    -B <x>         : use bit scores, sets min score to keep as <x>
+#    -S             : sort scores
 #
 # SRE, Wed Oct 28 14:03:52 1998
 # RCS $Id$
@@ -26,12 +27,14 @@ use infernal;
 
 $use_evalues   = 1;
 $use_bitscores = 0;
-$e_cutoff =   2;
+$e_cutoff =   10;
 $b_cutoff = 0.0;
+$sort_scores = 0;
 
-getopts('E:B:');
+getopts('E:B:S');
 if (defined $opt_E) { $e_cutoff = $opt_E; }
 if (defined $opt_B) { $b_cutoff = $opt_B; $use_evalues = 0; $use_bitscores = 1; }
+if (defined $opt_S) { $sort_scores = 1; }
 
 $output = join("",<>);
 &infernal::ParseINFERNAL($output);
@@ -54,30 +57,83 @@ if($use_evalues && (!$has_evalues))
     die("ERROR in infernal2glbf.pl, trying to use E-values but none reported.\n");
 }
 
-for ($i = 0; $i < $infernal::nhit; $i++)
+# if we're not sorting the scores, print them out in the order cmsearch reported them 
+if(!($sort_scores))
 {
-    if ((($use_bitscores) && $infernal::hitbitscore[$i] > $b_cutoff) ||
-	(($use_evalues)  && $infernal::hitevalue[$i]   < $e_cutoff))
+    for ($i = 0; $i < $infernal::nhit; $i++)
     {
-	#printf("%-24s %-6f\n", $infernal::targname[$i], $infernal::seqbitscore{$infernal::targname[$i]}); 
-	if($infernal::hitsqfrom[$i] > $infernal::hitsqto[$i])
+	if ((($use_bitscores) && $infernal::hitbitscore[$i] > $b_cutoff) ||
+	    (($use_evalues)  && $infernal::hitevalue[$i]   < $e_cutoff))
 	{
-	    #hit to reverse strand of query
-	    $orient = 1;
-	}
-	else
-	{
-	    $orient = 0;
-	}
-	if($use_evalues)
-	{
-	    printf("%-24s %-6f %d %d %d\n", $infernal::targname_byhit[$i], $infernal::hitevalue[$i], $infernal::hitsqfrom[$i], $infernal::hitsqto[$i], $orient); 
-	}
-	else
-	{
-	    printf("%-24s %-6f %d %d %d\n", $infernal::targname_byhit[$i], $infernal::hitbitscore[$i], $infernal::hitsqfrom[$i], $infernal::hitsqto[$i], $orient); 
+	    #printf("%-24s %-6f\n", $infernal::targname[$i], $infernal::seqbitscore{$infernal::targname[$i]}); 
+	    if($infernal::hitsqfrom[$i] > $infernal::hitsqto[$i])
+	    {
+		#hit to reverse strand of query
+		$orient = 1;
+	    }
+	    else
+	    {
+		$orient = 0;
+	    }
+	    if($use_evalues)
+	    {
+		printf("%-24s %-6f %d %d %d\n", $infernal::targname_byhit[$i], $infernal::hitevalue[$i], $infernal::hitsqfrom[$i], $infernal::hitsqto[$i], $orient); 
+	    }
+	    else
+	    {
+		printf("%-24s %-6f %d %d %d\n", $infernal::targname_byhit[$i], $infernal::hitbitscore[$i], $infernal::hitsqfrom[$i], $infernal::hitsqto[$i], $orient); 
+	    }
 	}
     }
-    
+}
+else # sort scores 
+{
+    for ($i = 0; $i < $infernal::nhit; $i++)
+    {
+	if($use_evalues)
+	{
+	    $sc_H{$i} = $infernal::hitevalue[$i];
+	}
+	else
+	{
+	    $sc_H{$i} = $infernal::hitbitscore[$i];
+	}
+    }
+    if($use_evalues)
+    {
+	@sorted_i_A = sort { $sc_H{$a} <=> $sc_H{$b} } (keys (%sc_H));
+    }
+    else
+    {
+	@sorted_i_A = sort { $sc_H{$b} <=> $sc_H{$a} } (keys (%sc_H));
+    }
+    for ($j = 0; $j < scalar(@sorted_i_A); $j++)
+    {
+	$i = $sorted_i_A[$j];
+	if ((($use_bitscores) && $infernal::hitbitscore[$i] > $b_cutoff) ||
+	    (($use_evalues)  && $infernal::hitevalue[$i]   < $e_cutoff))
+	{
+	    #printf("%-24s %-6f\n", $infernal::targname[$i], $infernal::seqbitscore{$infernal::targname[$i]}); 
+	    if($infernal::hitsqfrom[$i] > $infernal::hitsqto[$i])
+	    {
+		#hit to reverse strand of query
+		$orient = 1;
+	    }
+	    else
+	    {
+		$orient = 0;
+	    }
+	    if($use_evalues)
+	    {
+		printf("%-24s %-6f %d %d %d\n", $infernal::targname_byhit[$i], $infernal::hitevalue[$i], $infernal::hitsqfrom[$i], $infernal::hitsqto[$i], $orient); 
+	    }
+	    else
+	    {
+		printf("%-24s %-6f %d %d %d\n", $infernal::targname_byhit[$i], $infernal::hitbitscore[$i], $infernal::hitsqfrom[$i], $infernal::hitsqto[$i], $orient); 
+	    }
+	}
+    }
 }
 1;
+
+
--- benchmarks/cmsearch-rmark/rmark_MPI_cmsearch.pl
+++ benchmarks/cmsearch-rmark/rmark_MPI_cmsearch.pl
@@ -1,51 +1,36 @@
-#!/usr/local/bin/
+#!/usr/local/bin/perl
 #
-# Eric Nawrocki 10.09.05
-# rmark_clusterfy.pl
+# EPN, Mon Dec 11 14:27:41 2006
+# rmark_MPI_cmsearch.pl
 #
-# Prepare a RMARK benchmark for running on the cluster.
-# In non-MPI mode: split a RMARK run for X families in Y 
-# chromosomes into X*Y separate RMARK runs, one for each 
-# family/chromosome pair.
-# In MPI mode (currently only works with Infernal): 
-# Split a RMARK run for X families in however many chrom-
-# osomes into X separate 'mpirun' calls followed by
-# a bunch of infernal2glbf calls.
+# Prepare a MPI enabled cmsearch RMARK benchmark 
+# for running on the cluster.
 #
-# Usage: perl rmark_clusterfy.pl
+# Usage: perl rmark_MPI_cmsearch.pl
 #             <.rmm file name>
 #             <.rmk file name>
 #             <seq dir with *.ali *.idx *.test and *.raw files>
 #             <index file with fam names; provide path>
-#             <genome root X, X.fa, X.ebd, and X.chrlist must be in seq dir>
+#             <genome root X, X.fa, X.ebd must be in seq dir>
 #             <output file root>
 # Options:
-#        -E <x> : use E-values [default], set max E-val to keep as <x> [df: 2]
+#        -E <x> : use E-values [default], set max E-val to keep as <x> [df: 10]
 #        -B <x> : use bit scores, set min score to keep as <x>
-#        -P <x> : parallelize using MPI on <x> processors
-# Example:  perl rmark_clusterfy.pl infernal.rmm inf-71.rmk rmark-test/ rmark-test 
-#                                   inf-71
+# Example:  perl rmark_MPI_cmsearch.pl 100 infernal.rmm inf-71.rmk rmark-test/ rmark-test 
+#                                      inf-71
 #
 # The example run above will create the following:
 #     - a inf-71_rmark-test_out_dir directory with all the files needed to run
 #       rmark.pl copied to it (except seq files which stay in <seq dir>)
 #     - 
-# General Strategy (non-MPI mode)
-# (A) Create a script to execute on a cluster that will execute
-#     rmark.pl many times - namely one job for each family/chromosome
-#     pair in the full benchmark (SEE 'IMPORTANT 1:' below).
-# (B) Copy all required files into a new directory where the jobs 
-#     will be run (SEE 'IMPORTANT 2:' below).
-# (C) Create a shell script to post-process the results, that will run 
-#     after all the jobs have finished running.
-#
-# General Strategy (MPI mode)
-# (A) Create a script to submit X jobs (one for each family) to the 
+# General Strategy 
+# (A) Locally build the CMs necessary for running the benchmark.
+# (B) Create a script to submit X jobs (one for each family) to the 
 #     cluster to run cmsearch in MPI mode. 
-# (B) Copy all required files into a new directory where the jobs 
+# (C) Copy all required files into a new directory where the cmsearch jobs 
 #     will be run (SEE 'IMPORTANT 2:' below).
-# (C) Create a shell script to post-process the results, that will run 
-#     after all the jobs have finished running.
+# (D) Create a shell script to post-process the results, that will be run 
+#     locally after all the jobs have finished running.
 #
 # IMPORTANT: This script may need to be modified to suit the user's purposes. 
 #            See comments below that start with IMPORTANT X, where X is a number 
@@ -54,30 +39,29 @@
 require "sre.pl";
 use Getopt::Std;
 use Cwd;
-$e_cutoff = 2;
+$e_cutoff = 10;
 $b_cutoff = 0.0;
 $use_evalues   = 1;
 $use_bitscores = 0;
 
-getopts('E:B:P:');
+getopts('E:B:');
 if (defined $opt_E) { $e_cutoff = $opt_E; }
 if (defined $opt_B) { $b_cutoff = $opt_B; $use_evalues = 0; $use_bitscores = 1; }
 if (defined $opt_P) { $nprocs   = $opt_P; $do_mpi = 1; }
 
-$usage = "Usage: perl rmark_clusterfy.pl\n\t<.rmm file name>\n\t<.rmk file name>\n\t<dir with *.ali *.idx *.test and *.raw files>\n\t<index file with family names; provide path>\n\t<genome root X, X.chrlist, X.fa, X.ebd must be in seq dir>\n\t<output file root>\n";
+$usage = "Usage: perl rmark_MPI_cmsearch.pl\n\t<num processors to use>\n\t<.rmm file name>\n\t<.rmk file name>\n\t<dir with *.ali *.idx *.test and *.raw files>\n\t<index file with family names; provide path>\n\t<genome root X, X.fa, X.ebd must be in seq dir>\n\t<output file root>\n";
 $options_usage  = "\nOptions:\n\t";
 $options_usage .= "-E <x> : use E-values [default], set max E-val to keep as <x> [df: 2]\n\t";
-$options_usage .= "-B <x> : use bit scores, set min score to keep as <x>\n\t";
-$options_usage .= "-P <x> : parallelize using MPI on <x> processors\n\n";
+$options_usage .= "-B <x> : use bit scores, set min score to keep as <x>\n\n";
 
-if(@ARGV != 6)
+if(@ARGV != 7)
 {
     print $usage;
     print $options_usage;
     exit();
 }
 
-($rmm, $rmk, $seq_dir, $fam_idx, $genome_root, $out_file_root) = @ARGV;
+($nprocs, $rmm, $rmk, $seq_dir, $fam_idx, $genome_root, $out_file_root) = @ARGV;
 $seq_dir = getcwd() . "\/" . $seq_dir;
 
 $orig_rmk = $rmk;
@@ -92,12 +76,9 @@ if(! (-e "$run_dir/")) { system("mkdir $run_dir" ); }
 # IMPORTANT 2: Non-infernal may not require these files and may require other files, 
 #              delete those that are not needed and add new ones here!
 #
-$chrom_list    = $seq_dir . "/" . $genome_root . ".chrlist";
 $genome_file   = $seq_dir . "/" . $genome_root . ".fa";
 $embed_file    = $seq_dir . "/" . $genome_root . ".ebd";
 
-if(! (-e ("rmark.pl"))) { die("ERROR, rmark.pl must exist in the current directory."); } 
-else { system("cp rmark.pl $run_dir"); } 
 if(! (-e ("rmark_process_glbf.pl"))) { die("ERROR, rmark_process_glbf.pl must exist in the current directory."); } 
 else { system("cp rmark_process_glbf.pl $run_dir"); } 
 if(! (-e ("rmark_times.pl"))) { die("ERROR, rmark_process_glbf.pl must exist in the current directory."); } 
@@ -111,12 +92,17 @@ else { system("cp sre.pl $run_dir"); }
 
 if(! ( -e ("$genome_file"))) { die("ERROR, $genome_file must exist in $seq_dir") } 
 if(! ( -e ("$embed_file"))) { die("ERROR, $embed_file must exist in $seq_dir.") }
-if(! ( -e ("$chrom_list"))) { die("ERROR, $chrom_list must exist in $seq_dir.") }
 if(! ( -e ("$fam_idx"))) { die("ERROR, $fam_idx must exist in $seq_dir.") }
 if(! (-e ("$rmm"))) { die("ERROR, $rmm doesn't exist."); } 
 else { system("cp $rmm $run_dir"); }
 if(! (-e ("$rmk"))) { die("ERROR, $rmk doesn't exist."); }
 else { system("cp $rmk $run_dir"); }
+
+require("$orig_rmk");
+if($cms eq "" || $cmb eq "")
+{
+    die("ERROR, in MPI mode the RMARK config file $orig_rmk\nmust define variables \$cms and \$cmb\n");
+}
 
 #IMPORTANT, we've copied the $rmm and $rmk files, now make sure they
 # don't include a full path to the file, just the name
@@ -125,124 +111,55 @@ $rmk =~ s/.+\///;
 $fam_idx_root = $fam_idx;
 $fam_idx_root =~ s/.+\///;
 
-#if(! ( -e ("$genome_file"))) { die("ERROR, $genome_file must exist in the current directory.") }
-#else { system("cp $genome_file $run_dir"); } 
-#if(! ( -e ("$embed_file"))) { die("ERROR, $embed_file must exist in the current directory.") }
-#system("cp $embed_file $run_dir");
-#if(! ( -e ("$rmm"))) { die("ERROR, $rmm must exist in the current directory.") }
-#system("cp $rmm $run_dir");
-#if(! ( -e ("$rmk"))) { die("ERROR, $rmk must exist in the current directory.") }
-#system("cp $rmk $run_dir");
-#if(! ( -e ("$fam_idx"))) { die("ERROR, $fam_idx must exist in the current directory.") }
-#if(! ( -e ("$chrom_list"))) { die("ERROR, $chrom_list must exist in the current directory.") }
-
 # Copy any .prior files we might need.
 system("cp *.pri* $run_dir");
 # Copy any .null files we might need.
 system("cp *.null* $run_dir"); 
-    
-# Read in the roots of the chromosomes
-file_lines_to_arr($chrom_list, \@chrom_files_arr);
 
 # Read in the roots of the test families
 file_lines_to_arr($fam_idx, \@fam_roots_arr);
 
-if(!($do_mpi))
+# For each family, build a CM using the specifications in the 
+# RMARK module ($rmk) file
+for($i = 0; $i < scalar(@fam_roots_arr); $i++)
 {
-    # Create the script for the cluster that will submit
-    # all the jobs. 
-    push(@exec_lines, "#!/bin/sh");
-    # For each family...
-    for($i = 0; $i < scalar(@fam_roots_arr); $i++)
-    {
-	$fam = $fam_roots_arr[$i];
-	$num = $i + 1;
-	$index_file = "$run_dir/INDEX" . $num;
-	# Create a INDEX file for rmark.pl to read.
-	open(OUT, ">" . $index_file);
-	print OUT ("$fam\n");
-	close(OUT);
-	# For each chromosome...
-	for($j = 0; $j < scalar(@chrom_files_arr); $j++)
-	{
-	    #Determine the chromosome file, and check that it exists in the test_dir.
-	    $chrom_file = $chrom_files_arr[$j];
-	    if(! (-e ("$seq_dir" . "\/$chrom_file")))
-	    { die("ERROR, $chrom_file must exist in " . $seq_dir. "\n"); }
-	    $rmark_output = $out_file_root . "_" . $fam_roots_arr[$i] . "_" . $chrom_files_arr[$j];
-	    $cluster_index_file = $index_file;
-	    $cluster_index_file =~ s/.+\///;
-	    $job_name = "rm-$fam-$j";
-	    # Create the rmark.pl executing line for this family, this chromosome.
-	    if($use_evalues)
-	    {
-		$rmark_call = "rmark.pl -E " . $e_cutoff . " " . $rmm . " " . $rmk . " " . $seq_dir . " " . $cluster_index_file . " " . $chrom_files_arr[$j] . " " . $rmark_output;
-	    }
-	elsif($use_bitscores)
-	{
-	    $rmark_call = "rmark.pl -B " . $b_cutoff . " " . $rmm . " " . $rmk . " " . $seq_dir . " " . $cluster_index_file . " " . $chrom_files_arr[$j] . " " . $rmark_output;
-	}
-	    # Create a command the cluster will make to run rmark.pl
-	    # IMPORTANT 1: this is a version 6 SGE qsub command - works at Janelia Farm; 
-	    #              not sure about elsewhere...
-	    $exec_line = "qsub -N $job_name -o /dev/null -b y -cwd -V -j y perl " . $rmark_call;
-	    push(@exec_lines, $exec_line);
-	}
-    }	
-}
-elsif($do_mpi)
-{
-    require "$orig_rmk";
-
-    if($cms eq "")
-    {
-	die("ERROR, in MPI mode the RMARK config file $orig_rmk must define \$cms\n");
-    }
-    # Create the script for the cluster that will submit
-    # all the jobs. 
-    push(@exec_lines, "#!/bin/sh");
-    
     $fam = $fam_roots_arr[$i];
     $num = $i + 1;
-    $index_file = "$run_dir/INDEX" . $num;
-    # Create a INDEX file for rmark.pl to read.
-    open(OUT, ">" . $index_file);
-    print OUT ("$fam\n");
-    close(OUT);
-    # For each chromosome...
-    for($j = 0; $j < scalar(@chrom_files_arr); $j++)
+    $cm_file = $fam . ".cm";
+    $ali_file = $seq_dir . "/" . $fam . ".ali";
+    system("$cmb $fam.cm $ali_file > /dev/null");
+    #printf("$cmb $fam.cm $ali_file > /dev/null\n");
+    system("mv $fam.cm $run_dir");
+    #printf("mv $fam.cm $run_dir\n");
+}
+
+# Create the script for the cluster that will submit
+# the cmsearch jobs, one job for each family.
+push(@exec_lines, "#!/bin/sh");
+# For each family...
+for($i = 0; $i < scalar(@fam_roots_arr); $i++)
+{
+    $fam = $fam_roots_arr[$i];
+    $cm  = $fam . ".cm";
+    $job_name = "rm-$fam";
+    $cmsearch_name = "rm-$fam.cmsearch";
+    $out_name = "rm-$fam.out";
+    $err_name = "rm-$fam.err";
+
+    if($use_evalues)
     {
-	#Determine the chromosome file, and check that it exists in the test_dir.
-	$chrom_file = $chrom_files_arr[$j];
-	if(! (-e ("$seq_dir" . "\/$chrom_file")))
-	{ die("ERROR, $chrom_file must exist in " . $seq_dir. "\n"); }
-	$rmark_output = $out_file_root . "_" . $fam_roots_arr[$i] . "_" . $chrom_files_arr[$j];
-	$cluster_index_file = $index_file;
-	$cluster_index_file =~ s/.+\///;
-	$job_name = "rm-$fam-$j";
-	# Create the cmsearch call for this family, this chromosome.
-	if($use_evalues)
-	{
-	    $rmark_call = "rmark.pl -E " . $e_cutoff . " " . $rmm . " " . $rmk . " " . $seq_dir . " " . $cluster_index_file . " " . $chrom_files_arr[$j] . " " . $rmark_output;
-	}
-	elsif($use_bitscores)
-	{
-	    $rmark_call = "rmark.pl -B " . $b_cutoff . " " . $rmm . " " . $rmk . " " . $seq_dir . " " . $cluster_index_file . " " . $chrom_files_arr[$j] . " " . $rmark_output;
-	}
-	# Create a command the cluster will make to run rmark.pl
-	# IMPORTANT 1: this is a version 6 SGE qsub command - works at Janelia Farm; 
-	#              not sure about elsewhere...
-	$exec_line = "qsub -N $job_name -o /dev/null -b y -cwd -V -j y -pe lam-mpi-tight $nprocs \'" . $cmsearch_call . "\'";
-	push(@exec_lines, $exec_line);
+	$cmsearch_call = "mpirun C $cms -E $e_cutoff --noalign $cm $genome_file";
     }
-}	
+    elsif($use_bitscores)
+    {
+	$cmsearch_call = "mpirun C $cms -s $b_cutoff --noalign $cm $genome_file";
+    }	
 
+    $exec_line = "qsub -N $job_name -o $out_name -e $err_name -b y -cwd -V -j y -pe lam-mpi-tight $nprocs \'" . $cmsearch_call . " > $cmsearch_name\'";
+    push(@exec_lines, $exec_line);
+}
 
-
-
-
-
-
+    
 $command_file_name = $out_file_root . ".com";
 print_arr_to_file(\@exec_lines, $command_file_name);
 print_out_file_notice($run_dir . "/" . $command_file_name, "Command file with " . (scalar(@exec_lines)-1) . " qsub calls for the cluster.");
@@ -254,6 +171,17 @@ system("mv $command_file_name $run_dir");
 # all the jobs have finished running.
 $pp_file = $out_file_root . "_pp.script";
 open(PP, ">" . $pp_file);
+# First we need to get the glbf files:
+for($i = 0; $i < scalar(@fam_roots_arr); $i++)
+{
+    $fam = $fam_roots_arr[$i];
+    $cmsearch_name = "rm-$fam.cmsearch";
+    $glbf_name = "rm-$fam.glbf";
+    print PP ("perl infernal2glbf.pl $cmsearch_name > $glbf_name\n");
+}
+
+# Now we build the PP (post-processing) script the same as in
+# rmark_clusterfy.pl.
 print PP ("rm merged_" . $out_file_root . "*\n");
 $all_glbf_out = $out_file_root . "_all_glbf.concat";
 $all_time_out = $out_file_root . "_all_time.concat";
@@ -273,7 +201,6 @@ else
     $rmark_process_option = "B";
 }
 print PP ("perl rmark_process_glbf.pl $rmark_process_option $rmm $rmk $seq_dir $fam_idx_root $genome_root $all_glbf_out merged_" . $out_file_root . "_hit\n");
-
 
 # Here's some alternative rmark_process_glbf.pl calls :
 # the following 2 lines get results in 'fnt' and 'nnt' resolution modes 
