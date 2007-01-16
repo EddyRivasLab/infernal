c5dabb7d2d9abe93e89a5727513f181d9b84ad02
Eric Nawrocki <eric.nawrocki@gmail.com>
2007-01-16 16:50:16 +0000

    Modified the main for loops in the BandedCYKScan() recursion to go up to a precalc'ed 'curr_dmax' instead of the max of (W, dmax[v] and j). I was suprised to find a noticeable (~2%) speed increase.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/infernal/branches/0.7stable@1821 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- src/bandcyk.c
+++ src/bandcyk.c
@@ -918,6 +918,7 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
   int       gamma_j;            /* j index in the gamma matrix, which is indexed 0..j0-i0+1, 
 				 * while j runs from i0..j0 */
   int       gamma_i;            /* i index in the gamma* data structures */
+  int       curr_dmax;          /* temporary value for max d in for loops */
 
   /* EPN 08.11.05 Next line prevents wasteful computations when imposing
    * bands before the main recursion.  There is no need to worry about
@@ -1043,10 +1044,14 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
       prv = (j-1)%2;
       for (v = cm->M-1; v > 0; v--) /* ...almost to ROOT; we handle ROOT specially... */
 	{
+	  /* determine max d we're allowing for this state v and this position j */
+	  curr_dmax = (gamma_j < dmax[v]) ? gamma_j : dmax[v];
+	  if(curr_dmax > W) curr_dmax = W;
+
 	  if (cm->sttype[v] == D_st || cm->sttype[v] == S_st) 
 	    {
 	      if (cm->stid[v] == BEGL_S) jp = j % (W+1); else jp = cur;
-	      for (d = dmin[v]; (d <= dmax[v] && d <= gamma_j) && d <= W; d++) 
+	      for (d = dmin[v]; d <= curr_dmax; d++) 
 		{
 		  y = cm->cfirst[v];
 		  alpha[v][jp][d] = cm->endsc[v] + (cm->el_selfsc * (d-StateDelta(cm->sttype[v])));
@@ -1058,7 +1063,7 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
 	    }
 	  else if (cm->sttype[v] == MP_st) 
 	    {
-	      for (d = dmin[v]; (d <= dmax[v] && d <= gamma_j) && d <= W; d++)
+	      for (d = dmin[v]; d <= curr_dmax; d++)
 		{
 		  y = cm->cfirst[v];
 		  alpha[v][cur][d] = cm->endsc[v] + (cm->el_selfsc * (d-StateDelta(cm->sttype[v])));
@@ -1077,7 +1082,7 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
 	    }
 	  else if (cm->sttype[v] == ML_st || cm->sttype[v] == IL_st) 
 	    {
-	      for (d = dmin[v]; (d <= dmax[v] && d <= gamma_j) && d <= W; d++)
+	      for (d = dmin[v]; d <= curr_dmax; d++)
 		{
 		  y = cm->cfirst[v];
 		  alpha[v][cur][d] = cm->endsc[v] + (cm->el_selfsc * (d-StateDelta(cm->sttype[v])));
@@ -1096,7 +1101,7 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
 	    }
 	  else if (cm->sttype[v] == MR_st || cm->sttype[v] == IR_st) 
 	    {
-	      for (d = dmin[v]; (d <= dmax[v] && d <= gamma_j) && d <= W; d++)
+	      for (d = dmin[v]; d <= curr_dmax; d++)
 		{
 		  y = cm->cfirst[v];
 		  alpha[v][cur][d] = cm->endsc[v] + (cm->el_selfsc * (d-StateDelta(cm->sttype[v])));
@@ -1117,7 +1122,7 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
 	      w = cm->cfirst[v];
 	      y = cm->cnum[v];
 	      i = j-d+1;
-	      for (d = dmin[v]; (d <= dmax[v] && d <= gamma_j) && d <= W; d++) 
+	      for (d = dmin[v]; d <= curr_dmax; d++) 
 		{
 		  alpha[v][cur][d] = cm->endsc[v] + (cm->el_selfsc * (d - StateDelta(cm->sttype[v])));
 		  /* k is the length of the right fragment */
@@ -1149,7 +1154,12 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
        * by the way local alignment is parameterized (other transitions are
        * -INFTY), which is probably a little too fragile of a method. 
        */
-      for (d = dmin[0]; (d <= dmax[0] && d <= gamma_j) && d <= W; d++)
+
+      /* determine max d we're allowing for the root state and this position j */
+      curr_dmax = (gamma_j < dmax[0]) ? gamma_j : dmax[0];
+      if(curr_dmax > W) curr_dmax = W;
+
+      for (d = dmin[0]; d <= curr_dmax; d++)
 	{
 	  y = cm->cfirst[0];
 	  alpha[0][cur][d] = alpha[y][cur][d] + cm->tsc[0][0];
@@ -1191,7 +1201,7 @@ CYKBandedScan(CM_t *cm, char *dsq, int *dmin, int *dmax, int i0, int j0, int W,
       gback[gamma_j]  = -1;
       savesc[gamma_j] = IMPOSSIBLE;
       saver[gamma_j]  = -1;
-      for (d = dmin[0]; (d <= dmax[0] && d <= gamma_j) && d <= W; d++) 
+      for (d = dmin[0]; d <= curr_dmax; d++) 
 	{
 	  i = j-d+1;
 	  gamma_i = j-d+1-i0+1;
