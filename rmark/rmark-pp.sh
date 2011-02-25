897597eb2b2bb240cd3b8a9fcb1825e2dfb86353
Eric Nawrocki <eric.nawrocki@gmail.com>
2011-02-25 15:59:30 +0000

    rmark-pp.sh: 100K bootstrap samples are now done by default. Output 	     files are now named based on the scalar multiplier for 	     E-values, so multiple rmark-pp.sh's can be run for a 	     single benchmark, but with different E-value 	     multipliers.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/infernal/trunk@3492 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- rmark/rmark-pp.sh
+++ rmark/rmark-pp.sh
@@ -2,25 +2,28 @@
 # get running time
 ls $2/*.time   | perl rmark-time.pl > $2/$2.time
 # get MER
-cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | perl rmark-mer.pl $1.pos $2/$2.time > $2/$2.mer
+cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | perl rmark-mer.pl $1.pos $2/$2.time > $2/$2.em$3.mer
 # get ROC
-cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | rmark-rocplot -N 25000 --seed 181 $1 - > $2/$2.xy
+cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | rmark-rocplot -N 100000 --seed 181 $1 - > $2/$2.em$3.xy
 # get mer from rmark-rocplot
-cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | rmark-rocplot -N 25000 --mer --seed 181 $1 - > $2/$2.bmer
+cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | rmark-rocplot -N 100000 --mer --seed 181 $1 - > $2/$2.em$3.bmer
 # get numbers of false negatives and false positives at E-threshold of 0.1 from rmark-rocplot (after E-value inflation)
-cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | rmark-rocplot -N 25000 --Ethresh 0.1 --seed 181 $1 - > $2/$2.bEthresh
+#ARG=`echo 0.1/$3|bc -l`
+#echo 0.1/$3|bc -l
+cat $2/*out | perl rmark-multiply-evalues.pl $3 | sort -g | rmark-rocplot -N 100000 --Ethresh 0.1 --seed 181 $1 - > $2/$2.em$3.bEthresh
 
 # copy files to cwd
-cp $2/$2.mer ./
-cp $2/$2.bmer ./
+cp $2/$2.em$3.mer ./
+cp $2/$2.em$3.bmer ./
 cp $2/$2.time ./
-cp $2/$2.xy ./
+cp $2/$2.em$3.xy ./
 
 # summarize files to stdout
-echo -n $2 | awk '{printf("%-50s  ", $0)}' > $2/$2.sum
-cat $2/$2.bmer     | awk '{printf("MER:  %5s  %5s  %5s  %5s   ", $3, $7, $11, $15)}' >> $2/$2.sum
-cat $2/$2.bEthresh | awk '{printf("ETHRESH:0.1  %5s  %5s   ", $5, $9)}' >> $2/$2.sum
-grep ummary $2/$2.mer | awk '{print $7}' >> $2/$2.sum
-cp $2/$2.sum ./
-cat $2.sum
+echo -n $2 | awk '{printf("%-70s  ", $0)}' > $2/$2.em$3.sum
+echo -n $3 | awk '{printf("%4s  ", $0)}' >> $2/$2.em$3.sum
+cat $2/$2.em$3.bmer     | awk '{printf("MER:  %5s  %5s  %5s  %5s   ", $3, $7, $11, $15)}' >> $2/$2.em$3.sum
+cat $2/$2.em$3.bEthresh | awk '{printf("ETHRESH:0.1  %5s  %5s   ", $5, $9)}' >> $2/$2.em$3.sum
+grep ummary $2/$2.em$3.mer | awk '{print $7}' >> $2/$2.em$3.sum
+cp $2/$2.em$3.sum ./
+cat $2.em$3.sum
 
