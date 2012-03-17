9e56f8fa331355ba3730ad28c4dfb3f688bec53b
Eric Nawrocki <eric.nawrocki@gmail.com>
2012-03-17 18:47:13 +0000

    o rmark-master: added options to break down benchmark fasta file into   multiple files, each with 1 sequence. This will make benchmarking   the extremely slow --nohmm and --max options much easier.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/src/infernal/trunk@3946 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

--- rmark/rmark-master.pl
+++ rmark/rmark-master.pl
@@ -52,19 +52,21 @@
 # -X <s> : pass on -X <s> to the benchmark script
 # -Z     : pass on -Z to the benchmark script
 # -H <f> : pass on -H <f> to the benchmark script
+# -I <n> : requires x-cmsearch, do <n> searches, on <bmark_pfx>.1.fa through <bmark_pfx>.<n>.fa
 #
 # Examples of rmark benchmark:
 #   ./rmark-master.pl ../src/ cmsearch-results cmsearch-df.opts rmark3 ./rmark-cmsearch
 #   ./rmark-master.pl -P -N 1 ../src/ cmsearch-po-results cmsearch-df.opts rmark3 ./rmark-cmsearch
 
 use Getopt::Std;
-getopts('DPWB:N:T:FAM:O:C:X:ZYH:');
+getopts('DPWB:N:T:FAM:O:C:X:ZYH:I:');
 $do_posonly = 0;
 $do_poswindowonly = 0;
 $do_nopos = 0;
 $do_debug = 0;
 $do_onejob_only = 0;
 $do_build_models = 0;
+$do_indi = 0;
 $onejob = 0;
 $ncpu_set  = 0;
 $ncpu = 0;
@@ -90,6 +92,7 @@ if (defined $opt_C) { $do_fetch_models = 1; $master_model = $opt_C; if($do_build
 if (defined $opt_X) { $x_opt_to_pass = "-X $opt_X"; } 
 if (defined $opt_Z) { $z_opt_to_pass = "-Z"; } 
 if (defined $opt_H) { $h_opt_to_pass = "-H $opt_H"; } 
+if (defined $opt_I) { $do_indi = 1; $nindi = $opt_I; }
 if (defined $opt_M) { 
     $mpi_nprocs = $opt_M; 
     if($mpi_nprocs < 2 || $mpi_nprocs > 8) { die "ERROR, with -M <n>, <n> must be between 2 and 8"; }
@@ -110,8 +113,10 @@ $options_usage .= "-C <f> : fetch models from existing file <f>\n\t";
 $options_usage .= "-X <s> : pass on -X <s> to the benchmark script\n\t";
 $options_usage .= "-Z     : pass on -Z to the benchmark script\n\t";
 $options_usage .= "-H <f> : pass on -H <f> to the benchmark script\n\t";
+$options_usage .= "-I <f> : for x-cmsearch, do <n> searches, on <bmark_pfx>.1.fa through <bmark_pfx>.<n>.fa\n\t";
 $options_usage .= "-M <n> : pass -M <n> onto search module, telling it to run MPI with <n> <= 8 processors\n\t";
 $options_usage .= "         only valid if --mpi exists in <optsfile>\n\n";
+
 
 if(@ARGV != 7)
 {
@@ -189,6 +194,16 @@ if($do_nopos) {
     $fafile  = $npfafile;
     $posfile;
 }
+if($do_indi) { 
+    if($do_onejob_only) { die "-I doesn't work with -O"; }
+    if($rmark_script !~ m/x-cmsearch$/) { die "-I only works with x-cmsearch"; }
+    for($i = 1; $i <= $nindi; $i++) { 
+	$tmpfile = "$benchmark_pfx-1.fa";
+	if (! -e $tmpfile)  { die "with -I, $tmpfile doesn't exist"; }
+	$fafileA[($i-1)] = $tmpfile;
+    }
+}
+
 # Suck in the master table
 open(BENCHMARK_TBL, $tbl) || die;
 $n = 0;
@@ -259,17 +274,40 @@ if($do_debug) {
 }
 
 # Submit all the individual rmark jobs
-for ($i = 0; $i < $ncpu; $i++)
-{
-    if((!$do_onejob_only) || ($onejob == $i)) { 
-	if($do_mpi) { # turn exclusivity on, so we get all processors on our node, to run MPI with
-	    #printf("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge -l excl=true '$rmark_script $posonly_opt $debug_opt $build_opt $c_opt -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'\n");
-	    system("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge -l excl=true '$rmark_script $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'");
-	}
-	else { 
-	    #printf("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge '$rmark_script $posonly_opt $debug_opt $build_opt $c_opt $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'\n");
-	    system("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge -l short=true '$rmark_script $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'");
+if(! $do_indi) { 
+    for ($i = 0; $i < $ncpu; $i++)
+    {
+	if((!$do_onejob_only) || ($onejob == $i)) { 
+	    if($do_mpi) { # turn exclusivity on, so we get all processors on our node, to run MPI with
+		#printf("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge -l excl=true '$rmark_script $posonly_opt $debug_opt $build_opt $c_opt -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'\n");
+		system("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge -l excl=true '$rmark_script $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'");
+	    }
+	    else { 
+		#printf("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge '$rmark_script $posonly_opt $debug_opt $build_opt $c_opt $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'\n");
+		system("qsub -V -cwd -b y -N $resultdir.$i -j y -o $resultdir/tbl$i.sge -l short=true '$rmark_script $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.out'");
+	    }
 	}
     }
 }
-
+else { # $do_indi 
+    for ($i = 0; $i < $ncpu; $i++)
+    {
+	for($j = 0; $j < $nindi; $j++) { 
+	    $fafile = $fafileA[$j];
+	    if($do_mpi) { # turn exclusivity on, so we get all processors on our node, to run MPI with
+		# 2 qsubs, one with -T (--toponly) and one with -B (--bottomonly)
+		#printf("qsub -V -cwd -b y -N $resultdir.$i.$j.T -j y -o $resultdir/tbl$i.$j.T.sge -l excl=true '$rmark_script -K $j.T -T $posonly_opt $debug_opt $build_opt $c_opt -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.$j.T.out'\n");
+		#printf("qsub -V -cwd -b y -N $resultdir.$i.$j.B -j y -o $resultdir/tbl$i.$j.B.sge -l excl=true '$rmark_script -K $j.B -B $posonly_opt $debug_opt $build_opt $c_opt -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.$j.B.out'\n");
+		system("qsub -V -cwd -b y -N $resultdir.$i.$j.T -j y -o $resultdir/tbl$i.$j.T.sge -l excl=true '$rmark_script -K $j.T -T $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl$i $msafile $posfile $fafile $resultdir/tbl$i.$j.T.out'");
+		system("qsub -V -cwd -b y -N $resultdir.$i.$j.B -j y -o $resultdir/tbl$i.$j.B.sge -l excl=true '$rmark_script -K $j.B -B $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass -M $mpi_nprocs $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl$i $msafile $posfile $fafile $resultdir/tbl$i.$j.B.out'");
+	    }
+	    else { 
+		# 2 qsubs, one with -T (--toponly) and one with -B (--bottomonly)
+		#printf("qsub -V -cwd -b y -N $resultdir.$i.$j.T -j y -o $resultdir/tbl$i.$j.T.sge '$rmark_script -K $j.T -T $posonly_opt $debug_opt $build_opt $c_opt $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.$j.T.out'\n");
+		#printf("qsub -V -cwd -b y -N $resultdir.$i.$j.B -j y -o $resultdir/tbl$i.$j.B.sge '$rmark_script -K $j.B -B $posonly_opt $debug_opt $build_opt $c_opt $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.$j.B.out'\n");
+		system("qsub -V -cwd -b y -N $resultdir.$i.$j.T -j y -o $resultdir/tbl$i.$j.T.sge '$rmark_script -K $j.T -T $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.$j.T.out'");
+		system("qsub -V -cwd -b y -N $resultdir.$i.$j.B -j y -o $resultdir/tbl$i.$j.B.sge '$rmark_script -K $j.B -B $posonly_opt $poswindowonly_opt $debug_opt $build_opt $c_opt $x_opt_to_pass $z_opt_to_pass $h_opt_to_pass $execdir $scriptdir $modeldir $resultdir $optsfile $resultdir/tbl.$i $msafile $posfile $fafile $resultdir/tbl$i.$j.B.out'");
+	    }
+	}
+    }
+}
