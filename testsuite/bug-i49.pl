0568fd319c7ca4daba231a07391d0aa02e008e40
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2019-11-10 08:11:51 -0500

    Adds bug i49 in BUGTRAX and with test script integrated in testsuite and dev_testsuite. This was fixed as of 241987e but I just decided to add the test script.

---
+++ testsuite/bug-i49.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl
+
+# bug i49 - cmscan/cmsearch removes overlapping hits (--hmmonly)
+#
+# EPN, Sat Nov  9 16:43:00 2019
+#
+
+$usage = "perl bug-i49.pl <cmsearch> <path to bug-i49.cm> <path to bug-i49.fa>\n";
+if ($#ARGV != 2) { die "Wrong argument number.\n$usage"; }
+
+$cmsearch = shift;
+$cmfile   = shift;
+$seqfile  = shift;
+$ok       = 1;
+
+if ($ok) { 
+  $output = `$cmsearch --cpu 0 --toponly --tblout i49.tbl $cmfile $seqfile > /dev/null`;
+  if ($? != 0) { $ok = 0; }
+  # make sure there's two hits in the tab file (if the bug exists, there will be only 1 hit)
+  if(open(IN, "i49.tbl")) { 
+    $ok = 0;
+    $nhit = 0;
+    while($line = <IN>) { 
+      chomp $line;
+      if($line !~ m/^\#/) { $nhit++; }
+    }
+    close(IN);
+    if($nhit == 2) { $ok = 1; }
+  }
+  else { 
+    $ok = 0;
+  }
+}
+foreach $tmpfile ("i31.tbl") { 
+  unlink $tmpfile if -e $tmpfile;
+}
+
+if ($ok) { print "ok\n";     exit 0; }
+else     { print "FAILED\n"; exit 1; }
+
+
