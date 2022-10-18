a22578215e834557db16d9cb6e46d9728ac134df
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2022-10-18 14:23:07 -0400

    Removes check for cp9mx size, so as not to deviate from 1.1x behavior in place since 2013

--- src/hmmband.c
+++ src/hmmband.c
@@ -410,6 +410,7 @@ cp9_IterateSeq2Bands(CM_t *cm, char *errbuf, ESL_DSQ *dsq, int64_t i0, int64_t j
 {
   int   status;
   int   do_trunc = cm_pli_PassAllowsTruncation(pass_idx);
+  /* sizes of matrices, note enforce hbmx_Mb < size_limit, not tot_Mb < size_limit */
   float cp9mx_Mb = 0.; /* approximate size in Mb required for CP9 matrices */
   float hbmx_Mb;       /* approximate size in Mb required for HMM banded matrix */
   float tot_Mb;        /* cp9mx_Mb + hbmx_Mb */
@@ -428,10 +429,6 @@ cp9_IterateSeq2Bands(CM_t *cm, char *errbuf, ESL_DSQ *dsq, int64_t i0, int64_t j
       else         { status = cm_AlignSizeNeededHB  (cm, errbuf, j0-i0+1, size_limit, do_sample, do_post, NULL, NULL, NULL, &cp9mx_Mb, &hbmx_Mb, &tot_Mb); }
       if(status != eslOK && status != eslERANGE) return status;
     }      
-    if(cp9mx_Mb > size_limit) { 
-      /* the CP9 matrices alone exceed our size limit, so even if hbmx_Mb was zero Mb we'd still be too big, exit with informative error message */
-      ESL_FAIL(eslERANGE, errbuf, "HMM matrices for banded alignment need %.2f Mb > %.2f Mb limit.\nUse --mxsize, --maxtau or --tau.", cp9mx_Mb, (float) size_limit);
-    }
     /*printf("cm->tau: %10.2g thresh1: %4.2f thresh2: %4.2f mxsize: %.2f\n", cm->tau, cm->cp9b->thresh1, cm->cp9b->thresh2, hbmx_Mb);*/
     /* check if we can stop iterating, 4 ways we can
      * case 1: matrix is now smaller than our limit.
